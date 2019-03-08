c62b0f370a6efeddc3271edfe0a670a1c9a69322
moodyRahman <mrahman01@stuy.edu>
2019-03-08 15:43:06 -0500

    debugger for input parser

--- USACO.java
+++ USACO.java
@@ -7,12 +7,34 @@ public class USACO{
 
 public static int bronze(String fname){
 	ArrayList<String> temp = new ArrayList<String>();
+	String[] rawfile = null;
 	try {
-		USACO.reader(fname, temp);
+		rawfile = USACO.reader(fname, temp);
 	} catch(FileNotFoundException e) {
 		System.out.println("NOT A VALID FILE >:(");
 		System.exit(0);
 	}
+
+	String[] rawinp = rawfile[0].split(" ");
+	String[] rawdata = new String[rawfile.length - 1];
+	for (int x = 0; x < rawdata.length; x++) {
+		rawdata[x] = rawfile[x+1];
+	}
+
+	for (String s : rawfile) {
+		System.out.println(s);
+	}
+	System.out.println();
+	for (String s : rawdata) {
+		System.out.println(s);
+	}
+
+	System.out.println();
+	for (String s : rawinp) {
+		System.out.println(s);
+	}
+
+
 
 
 
@@ -30,6 +52,11 @@ private static String[] reader(String fname, ArrayList<String> boop) throws File
 	// String[][] br =
 	boop.toArray(b);
 	return b;
+}
+
+
+public static void main(String[] args) {
+	USACO.bronze("testcases/makelake.1.in");
 }
 
 

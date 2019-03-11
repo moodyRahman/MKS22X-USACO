94b2ddfeb649e573108e7b064870d97d18dd353e
moodyRahman <mrahman01@stuy.edu>
2019-03-11 19:52:55 -0400

    code nuke

--- USACO.java
+++ USACO.java
@@ -6,31 +6,6 @@ import java.util.ArrayList;
 public class USACO{
 
 public static int bronze(String fname){
-	ArrayList<String> temp = new ArrayList<String>();
-	String[] rawfile = null;
-	try {
-		rawfile = USACO.reader(fname, temp);
-	} catch(FileNotFoundException e) {
-		System.out.println("NOT A VALID FILE >:(");
-		System.exit(0);
-	}
-
-	String[] rawinp = rawfile[0].split(" ");
-	String[] rawdata = new String[rawfile.length - 1];
-	for (int x = 0; x < rawdata.length; x++) {
-		rawdata[x] = rawfile[x+1];
-	}
-
-	ArrayList<String[]> dr = new ArrayList<String[]>();
-	for (int x = 0; x < rawdata.length; x++) {
-		dr.add(rawdata[x].split(" "));
-	}
-
-	for (String[] st : dr) {
-		for (String se : st) {
-			System.out.println(se);
-		}
-	}
 
 
 

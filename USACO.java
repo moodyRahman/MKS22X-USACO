1c744dfedd2cfd2b47333a8b3b4e4cb0eb9e1ee3
moodyRahman <mrahman01@stuy.edu>
2019-03-11 19:56:37 -0400

    into formatted inputs

--- USACO.java
+++ USACO.java
@@ -6,6 +6,20 @@ import java.util.ArrayList;
 public class USACO{
 
 public static int bronze(String fname){
+	ArrayList<String> temp = new ArrayList<String>();
+	String[] rawfile = null;
+	try {
+		rawfile = USACO.reader(fname, temp);
+	} catch(FileNotFoundException e) {
+		System.out.println("NOT A VALID FILE >:(");
+		System.exit(0);
+	}
+
+	String[] rawInput = rawfile[0].split(" ");
+	int[] formatted_inputs = new int[4];
+	for (int x = 0; x < 4; x++) {
+		formatted_inputs[x] = Integer.parseInt(rawInput[x]);
+	}
 
 
 
